6b5917c8a462c5fbe5a838b499b07814b02c949a
Marco <marco.salman@gmail.com>
2022-02-24 20:29:39 -0500

    pair programming wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,43 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// const transpose = function(matrix) {
+//   let array = [];
+//   for (let i = 0; i < matrix[0].length; i++) {
+//   array.push([]);
+//   }
+  
+//   for (let row = 0; row < matrix.length; row++) {
+//     for (let col = 0; col < matrix[row].length; col++) {
+//         array[col][row] = matrix[row][col];
+//     } 
+//   }
+//   return array;
+//   };
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+
+    let resultMatrix = [];
+    for (let r = 0; r < matrix.length; r++) {
+      for (let c = 0; c < matrix[r].length; c++) {
+        // check if resultMatrix exists, if not, create it. Otherwise proceed and push the number into it.
+        if (!resultMatrix[c]) {
+          resultMatrix.push([]);
+        }
+        resultMatrix[c].push(matrix[r][c]);
+      }
+    }
+    return resultMatrix;
+  };
+  
+  const wordSearch = (letters, word) => { 
+      let t = transpose(letters);
+      const horizontalJoin = letters.map(ls => ls.join(''))
+      const verticalJoin = t.map(ls => ls.join(''));
+      for (l of horizontalJoin) {
+          if (l.includes(word)) return true
+      }
+      for (v of verticalJoin) {
+          if (v.includes(word)) return true
+      }
+      return false;
+  }
+  
+  module.exports = wordSearch
